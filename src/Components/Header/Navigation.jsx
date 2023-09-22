f8574c6905425d560dd10848dddd1c1f8bf88ecf
Alihaider0315 <ah712200@gmail.com>
2023-09-22 17:21:10 +0500

    Code Updated.

---
+++ src/Components/Header/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <div>
+    <NavLink className="mr-5 hover:text-gray-900" to="/">Home</NavLink>
+    <NavLink className="mr-5 hover:text-gray-900" to="/about">About Us</NavLink>
+    <NavLink className="mr-5 hover:text-gray-900" to="/product">Our Shop</NavLink>
+    <NavLink className="mr-5 hover:text-gray-900" to="/contact">Contact Us</NavLink>
+    </div>
+  )
+}
+
+export default Navigation
---
+++ src/Router/Router.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Routes , Route} from "react-router-dom";
+import About from '../Pages/About';
+import Contact from '../Pages/Contact';
+import Home from '../Components/Hero/Hero'
+import Product from '../Components/Product/Product';
+import Singleproduct from '../Components/SingleProduct/Singleproduct';
+
+
+const Router = () => {
+  return (
+   <Routes>
+   <Route path='/' element={<Home/>}></Route>
+   <Route path='/about' element={<About/>}></Route>
+   <Route path='/product' element={<Product/>}></Route>
+   <Route path='/contact' element={<Contact/>}></Route>
+   <Route path='/singleproduct/:id' element={<Singleproduct/>}></Route>
+   </Routes>
+  )
+}
+
+export default Router
